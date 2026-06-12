50a76cc0e6e0a35951acbe64824ee15493590848
Gary Tou <gary@garytou.com>
2026-06-12 15:13:22 -0700

    [Healthcheck] Also check DB connection

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import twilio from 'twilio';
 import { receiver as slackReceiver } from './utils/slack';
 import { handleTwilioMessage } from './helpers/mfa';
 import sourceCommit from './helpers/sourceCommit';
+import prisma from './utils/prisma';
 
 const TWILIO_NO_REPLY = '<Response></Response>';
 
@@ -21,17 +22,32 @@ router.get('/', (req: Request, res: Response) => {
 	res.redirect('https://github.com/hackclub/mfa');
 });
 
-// Ping Pong (test endpoint)
-router.get('/ping', async (req: Request, res: Response) => {
-	let body = 'pong! 🏓';
+// Health check endpoint. `/ping` is kept as an alias for backwards compat.
+const healthCheck = async (req: Request, res: Response) => {
+	let body = 'healthy!';
 
 	const hash = await sourceCommit();
 	if (hash) {
 		body += `\n\nBuild ${hash.slice(0, 7)}`;
 	}
 
+	// Verify the application can reach the database. A lightweight `SELECT 1`
+	// confirms both connectivity and that we can execute queries.
+	try {
+		await prisma.$queryRaw`SELECT 1`;
+	} catch (err) {
+		console.error('Healthcheck failed: unable to reach the database', err);
+		res
+			.status(500)
+			.send(`${body}\n\nUnhealthy: unable to connect to the database`);
+		return;
+	}
+
 	res.send(body);
-});
+};
+
+router.get('/up', healthCheck);
+router.get('/ping', healthCheck);
 
 // Receive incoming SMS from Twilio
 // https://www.twilio.com/docs/messaging/guides/webhook-request
